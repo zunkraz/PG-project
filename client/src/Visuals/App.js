2ea010312f7bb238231a95eb7f1f1eb832b6b975
Gaston Scocco <gaston_scocco@hotmail.com>
2021-10-25 15:00:07 -0300

    Init Commit

--- client/src/Visuals/App.js
+++ client/src/Visuals/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './Components/Footer/Footer';
+// Footer for test
+
 
 function App() {
   return (
     <div className="App">
-    hola mundo
+      hola mundo
+      <Footer/>
     </div>
   );
 }
