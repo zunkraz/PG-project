e6f98b6682afb80b4782cfeb7fb6bbea58cfd0c4
Agustin Ecker <amecker@hotmail.com>
2021-10-26 18:34:36 -0300

    agregado form profesional y terminadas validaciones

--- client/src/Visuals/Components/Register/validations.js
+++ client/src/Visuals/Components/Register/validations.js
@@ -1,36 +1,25 @@
-const fakeusers=[{
-  username: "amigo",
-  email: "sad@sad.com",
-  password:"1234"
-},
-{
-  username: "carlos",
-  email: "bilardo@sad.com",
-  password:"12346"
-}
-]
-
-
 export default function validate(newUser, setError){
     let errors = {};
     let isValid = true;
 
-    if (fakeusers.find(user=>user.username===newUser["username"])) {
-      isValid = false;
-      errors["username"] = "El nombre de usuario ya existe";
-    }
-
-    if (fakeusers.find(user=>user.email===newUser["email"])) {
-      isValid = false;
-      errors["email"] = "El correo electronico ya esta registrado";
-    }
-
-    if (typeof newUser["email"] !== "undefined") {
+    if (typeof newUser["email"] !== undefined) {
         
       var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
       if (!pattern.test(newUser["email"])) {
         isValid = false;
         errors["email"] = "Por favor ingresa un email válido";
+      }
+    }
+    if (typeof newUser["name"] !== undefined && typeof newUser["lastName"] !== undefined) {
+        
+      var patternname = new RegExp( /^[a-zA-Z ]{2,30}$/);
+      if (!patternname.test(newUser["name"])) {
+        isValid = false;
+        errors["name"] = "Por favor ingresa un nombre válido";
+      }
+      if (!patternname.test(newUser["lastName"])) {
+        isValid = false;
+        errors["name"] = "Por favor ingresa un nombre válido";
       }
     }
 
@@ -40,7 +29,19 @@ export default function validate(newUser, setError){
         isValid = false;
         errors["password"] = "Las contraseñas no coinciden.";
       }
+      if (newUser["password"].length<6) {
+        isValid = false;
+        errors["password"] = "La contraseña debe contener al menos 6 caracteres";
+      }
     } 
+    if (typeof newUser["telNum1"] !== undefined) {
+      var patternphone = new RegExp( /^[0-9]{8,30}$/);
+      if (!patternphone.test(newUser["telNum1"])) {
+        isValid = false;
+        errors["phone"] = "Por favor ingresa un telefono válido";
+      }
+    }
+
 
     setError(errors)
     return isValid;
