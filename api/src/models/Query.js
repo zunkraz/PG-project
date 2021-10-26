507adc657ed2583d719cec63748ac5ee94bac9a0
adriadria <orochimei11@gmail.com>
2021-10-26 19:37:44 -0300

    models/Query: fixed typos

--- api/src/models/Query.js
+++ api/src/models/Query.js
@@ -6,7 +6,7 @@ const query = mongoose.Schema(
             type: Number,
             required: true,
         },
-        costumerId: {
+        customerId: {
             type: Schema.Types.ObjectId,
             required: true,
         },
