0a9c2b8417898598629b564a073f798ab3b6a4d0
Agustin Ecker <amecker@hotmail.com>
2021-10-26 20:48:46 -0300

    readded files

--- client/src/Visuals/Containers/Pages/Register.jsx
+++ client/src/Visuals/Containers/Pages/Register.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, {useState} from "react";
+import RegisterFormUser from "../../Components/Register/RegisterFormUser";
+import RegisterFormPro from "../../Components/Register/RegisterFormPro";
 
-function Register() {
-    return (
-        <div>
-            Register Components Here
+export default function Register(){
+    const [active, setActive] = useState("cliente");
+
+    function handleClick(e){
+        const name= e.target.name
+        if(name==="cliente") setActive("cliente")
+        if(name==="profesional") setActive("profesional")
+    }
+    return(
+        <div class="uk-background-muted">
+            <h2 class="uk-margin-left uk-padding">Registrarse</h2>
+            <div class="uk-grid">
+                <button class="uk-button uk-button-danger uk-width-1-2" name="cliente" onClick={handleClick}>Nuevo Cliente</button>
+                <button class="uk-button uk-button-danger uk-width-1-2" name="profesional" onClick={handleClick}>Nuevo Profesional</button>
+            </div>
+            {active==="cliente" && <RegisterFormUser/>}
+            {active==="profesional" && <RegisterFormPro/>}
         </div>
+        
+        
     )
 }
-
-export default Register
