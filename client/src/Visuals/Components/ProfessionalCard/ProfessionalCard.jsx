ffd524acd1e747966620aa995c3116e84d7e2a24
Agustin Ecker <amecker@hotmail.com>
2021-10-27 19:14:59 -0300

    creado componente card y su container lista, no terminado

---
+++ client/src/Visuals/Components/ProfessionalCard/ProfessionalCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { thumbsup } from "../../Assets/icons";
+
+
+export default function ProfessionalCard({id, name, lastName, category, likes, img}){
+    return (
+        <div class="w-80 h-96 rounded overflow-hidden shadow-lg m-10">
+            <Link to={`/professionals/${id}`} style={{ "textDecoration": "none" }}>
+            <div class="h-1/2" >
+                <img class="object-contain h-48 w-full" src={img} alt=""/>
+            </div>
+            <div class="h-1/4 px-6 py-4 space-y-2">
+                <h4 class="font-bold text-xl mb-2">{name} {lastName}</h4>
+                <p> {category} </p>
+            </div>
+            <div class="h-1/6 object-bottom px-6 py-4 space-y-2 flex flex-row justify-between content-end">
+                    <p class="inline-flex"> {thumbsup} {likes}</p> 
+                    <p class="inline-flex">10$ por hora</p>
+            </div>
+            </Link>
+        </div>
+        
+    )
+}
+
---
+++ client/src/Visuals/Components/ProfessionalCard/ProfessionalsCatalog.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import ProfessionalCard from "./ProfessionalCard";
+
+export default function ProfesionalsCatalog(){
+    return (
+        <ul class="flex flex-wrap">
+        {professionals.map(p=>{
+            return(<li key={professionals.indexOf(p)}>
+                <ProfessionalCard key={professionals.indexOf(p)}
+                id={professionals.indexOf(p)}
+                name={p.name}
+                lastName= {p.lastName}
+                img={p.img}
+                category= {p.category}
+                likes= {p.likes}
+                />
+
+            </li>)
+        })}
+        </ul>
+    )
+}
+
+const professionals = [
+    {
+        name: "Ricardo",
+        lastName: "Montaner",
+        category: "Arquitecto",
+        likes: 25,
+        dislikes: 4,
+        img: "https://media.istockphoto.com/photos/confident-businessman-portrait-isolated-picture-id455586761?s=612x612",
+    },
+    {
+        name: "Paulina",
+        lastName: "Rubio",
+        category: "Profesora",
+        likes: 75,
+        dislikes: 14,
+        img: "https://media.istockphoto.com/photos/portrait-of-young-cheerful-african-american-woman-picture-id1207862195?s=612x612",
+    },
+    {
+        name: "Ricardo",
+        lastName: "Arjona",
+        category: "Abogado",
+        likes: 23,
+        dislikes: 8,
+        img: "https://media.istockphoto.com/photos/portrait-of-serious-mid-adult-man-picture-id805011368?s=612x612",
+    },
+    {
+        name: "Andres",
+        lastName: "Cepeda",
+        category: "Contador",
+        likes: 84,
+        dislikes: 21,
+        img: "https://media.istockphoto.com/photos/mature-businessman-portrait-isolated-picture-id455586203?s=612x612",
+    },
+    {
+        name: "Julieta",
+        lastName: "Venegas",
+        category: "Gamer",
+        likes: 61,
+        dislikes: 19,
+        img: "https://media.istockphoto.com/photos/m-taking-this-business-to-the-top-picture-id460078235?s=612x612",
+    },
+];
