8927ab9a65884fafef58d341c79e67ac8242487c
juanpalozano <jlozano@urbanodigital.com>
2021-10-27 10:16:45 -0500

    Componentes Home

---
+++ client/src/Visuals/Components/FeaturedProfessionals/FeaturedProfessional.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const FeaturedProfessional = ({data}) => {
+    return (
+        <div className="col-1-5@xl padd-lg">
+            <div className="">
+                <img src={data.img}/>
+            </div>
+            <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                <div className="position-middle-parent">
+                    <div className="icon-xl border-radius-sm bg-color-main flex-center"><span>Icon</span></div>
+                </div> 
+                <div className="text-bold text-center font-lg font-main">{data.name} {data.lastName}</div>
+                <div className="text-bold text-center">{data.category}</div>
+                <div className="mrg-lg-t flex-center">
+                    <div className="padd-lg-r">Like {data.likes}</div>
+                    <div className="padd-lg-l">Dislike {data.dislikes}</div>
+                </div>
+                <div className="mrg-lg-t padd-md border-radius-sm action action-professional">
+                    Ver Profesional
+                </div>
+            </div>
+        </div>
+    );
+}
+export default FeaturedProfessional;
---
+++ client/src/Visuals/Components/FeaturedProfessions/FeaturedProfessions.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+
+const FeaturedProfessions = () => {
+    return (
+        <div>
+            <div className="wrapper padd-xl bg-color-dark-a20">
+                <section>
+                    <div className="text-bold font-main font-2x">
+                        Profesiones Destacadas
+                    </div>
+                    <div className="mrg-lg-t">
+                    Maecenas facilisis diam orci, quis iaculis risus suscipit eu. Phasellus fringilla iaculis lacus at dapibus. Mauris et augue a nibh elementum suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut elementum urna vitae odio faucibus, a interdum lacus dictum. Integer convallis accumsan ornare. Curabitur convallis nibh non imperdiet volutpat.
+                    </div>
+                </section>
+            </div>
+            <div className="wrapper padd-xl-tb bg-color-dark-a40">
+                <section>
+                    <div className="col-1-4@xl padd-lg">
+                        <div className="ratio-3-2 bg-color-light"></div>
+                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                            <div className="position-middle-parent">
+                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
+                            </div>                            
+                            Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
+                        </div>
+                    </div>
+                    <div className="col-1-4@xl padd-lg">
+                        <div className="ratio-3-2 bg-color-light"></div>
+                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                            <div className="position-middle-parent">
+                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
+                            </div>                            
+                            Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
+                        </div>
+                    </div>
+                    <div className="col-1-4@xl padd-lg">
+                        <div className="ratio-3-2 bg-color-light"></div>
+                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                            <div className="position-middle-parent">
+                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
+                            </div>                            
+                            Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
+                        </div>
+                    </div>
+                    <div className="col-1-4@xl padd-lg">
+                        <div className="ratio-3-2 bg-color-light"></div>
+                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                            <div className="position-middle-parent">
+                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
+                            </div>                            
+                            Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+}
+
+export default FeaturedProfessions;
