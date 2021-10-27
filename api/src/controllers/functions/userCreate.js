a88f91cb5beaa29cd6eab77cc27d812f98e6aa00
Mauricio Portillo <maujpok@gmail.com>
2021-10-27 11:08:09 -0300

    Update userCreate.js

--- api/src/controllers/functions/userCreate.js
+++ api/src/controllers/functions/userCreate.js
@@ -1,31 +1,34 @@
 const User = require('../../models/User');
 
-module.exports = async (req, res) => {
-    const body = req.body
-    const queryUsername = await User.findOne({username: body.username})
-    const queryEmail = await User.findOne({email: body.email})
+module.exports = async (req, res, next) => {
+    
+    try{
+        const body = req.body
+        const queryUsername = await User.findOne({username: body.username})
+        const queryEmail = await User.findOne({email: body.email})
 
-    if(queryUsername === null && queryEmail === null) {
-        const {name, lastname, username, email, password, professionalRegistration, country, isAdmin, isProfesional} = body;
+        if(queryUsername === null && queryEmail === null) {
+            const {name, lastname, username, email, password, professionalRegistration, country, isAdmin, isProfesional} = body;
 
-        let newUser = {
-            name,
-            lastname,
-            username,
-            email,
-            password,
-            professionalRegistration,
-            country,
-            isAdmin,
-            isProfesional
-        };
-        
-        await User.insertOne(newUser);
-        res.send(newUser)
-    }
-    else if(queryUsername !== null) {
-        res.status(400).send("El usuario ya existe")
-    } else {
-        res.status(400).send("El email ingresado ya existe")
-    }
+            let newUser = {
+                name,
+                lastname,
+                username,
+                email,
+                password,
+                professionalRegistration,
+                country,
+                isAdmin,
+                isProfesional
+            };
+
+            await User.save(newUser);
+            res.send(newUser)
+         else if(queryUsername !== null) {
+            res.status(400).send("El usuario ya existe")
+        } else {
+            res.status(400).send("El email ingresado ya existe")
+        }
+    }catch(err){
+        next(err)
 };
