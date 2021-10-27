ea6b59e98948a5ae13829d67bc585e4f2bfa16b8
antonio <zunkraz0701@gmail.com>
2021-10-26 20:18:24 -0400

    componente slogan sin estilizar

--- client/src/Visuals/Containers/Pages/Home.jsx
+++ client/src/Visuals/Containers/Pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Slogan from '../../Components/SloganComponents/Slogan'
 
 
 function Home() {
     return (
         <div>
-            HOME COMPONENTS HERE
+            <Slogan />
         </div>
     )
 }
