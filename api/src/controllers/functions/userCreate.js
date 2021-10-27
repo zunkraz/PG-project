a5140f3d07dec2c35b8746ed53117caaa1bb211a
Mauricio Portillo <maujpok@gmail.com>
2021-10-27 10:17:22 -0300

    ruta users, funcion userCreate, middl en index de rutas

---
+++ api/src/controllers/functions/userCreate.js
@@ -0,0 +1,31 @@
+const User = require('../../models/User');
+
+module.exports = async (req, res) => {
+    const body = req.body
+    const queryUsername = await User.findOne({username: body.username})
+    const queryEmail = await User.findOne({email: body.email})
+
+    if(queryUsername === null && queryEmail === null) {
+        const {name, lastname, username, email, password, professionalRegistration, country, isAdmin, isProfesional} = body;
+
+        let newUser = {
+            name,
+            lastname,
+            username,
+            email,
+            password,
+            professionalRegistration,
+            country,
+            isAdmin,
+            isProfesional
+        };
+        
+        await User.insertOne(newUser);
+        res.send(newUser)
+    }
+    else if(queryUsername !== null) {
+        res.status(400).send("El usuario ya existe")
+    } else {
+        res.status(400).send("El email ingresado ya existe")
+    }
+};
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,8 +4,10 @@ const router = Router();
 //Aquí importamos todas las rutas, solo puse un ejemplo
 const professionals = require('./professionals');
 const tips = require('./tips');
+const users = require('./users');
 
 router.use('/tips',tips);
 router.use('/professionals', professionals);
+router.use('./users', users);
 
 module.exports = router;
