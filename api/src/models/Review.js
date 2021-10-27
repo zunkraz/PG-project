3da587bb1538eb1faabd549a39afcba6bba2a243
adriadria <orochimei11@gmail.com>
2021-10-27 14:41:24 -0300

    feat/models: adding Review model

---
+++ api/src/models/Review.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const review = new Schema(
+    {
+        rate: {
+            type: Number,
+            required: true,
+        },
+        userId: {
+            type: Schema.Types.ObjectId, 
+            ref: 'users',
+            required: true,
+        },
+        text: {
+            type: String,
+        },       
+    },
+    { timestamp: true }
+)
+
+const Review = mongoose.model('Reviews', review);
+
+module.exports = Review;
