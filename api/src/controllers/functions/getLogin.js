eed042856bd8b0498bb6bdf5a4f4497f6cbaff0b
Nohelia <nohelia.j.r.a@gmail.com>
2021-10-28 13:07:00 -0400

    ruta de chequeo login

---
+++ api/src/controllers/functions/getLogin.js
@@ -0,0 +1,25 @@
+const User = require('./../../models/User')
+
+module.exports = async (req,res,next) => {
+    try{
+        const {username,password} = req.query;
+        let user = await User.findOne({username:username}, 'password')
+
+        let msg 
+        let stat
+        if(password === user.password){
+            msg = 'Correcto'
+            stat = 200
+        }else{
+            msg = 'Las contraseñas no coinciden'
+            stat = 400
+        } 
+        res.status(stat).send(msg)
+    }catch(err){
+        if(err.message.includes('null')){
+            next({message:'User not found',status:400})
+        }else{
+            next(err)
+        }
+    }
+}
