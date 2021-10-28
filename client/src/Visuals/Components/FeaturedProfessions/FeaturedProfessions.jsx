f91d35d54a55462a342f38b168b09bfdb5d123cf
juanpalozano <jlozano@urbanodigital.com>
2021-10-28 09:05:30 -0500

    Home and components

--- client/src/Visuals/Components/FeaturedProfessions/FeaturedProfessions.jsx
+++ client/src/Visuals/Components/FeaturedProfessions/FeaturedProfessions.jsx
@@ -11,13 +11,24 @@ const FeaturedProfessions = () => {
                         Profesiones Destacadas
                     </div>
                     <div className="mrg-lg-t">
-                    Maecenas facilisis diam orci, quis iaculis risus suscipit eu. Phasellus fringilla iaculis lacus at dapibus. Mauris et augue a nibh elementum suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut elementum urna vitae odio faucibus, a interdum lacus dictum. Integer convallis accumsan ornare. Curabitur convallis nibh non imperdiet volutpat.
+                        Maecenas facilisis diam orci, quis iaculis risus suscipit eu. Phasellus fringilla iaculis lacus at dapibus. Mauris et augue a nibh elementum suscipit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut elementum urna vitae odio faucibus, a interdum lacus dictum. Integer convallis accumsan ornare. Curabitur convallis nibh non imperdiet volutpat.
                     </div>
                 </section>
             </div>
             <div className="wrapper padd-xl-tb bg-color-dark-a40">
                 <section>
-                    <div className="col-1-4@xl padd-lg">
+                    <div className="col-1-4@xl col-1-4@lg col-1-4@md padd-lg">
+                        <div className="ratio-3-2 bg-color-light"></div>
+                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
+                            <div className="position-middle-parent">
+                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
+                            </div>
+                            <div>
+                                Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-1-4@xl col-1-4@lg col-1-4@md padd-lg">
                         <div className="ratio-3-2 bg-color-light"></div>
                         <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
                             <div className="position-middle-parent">
@@ -26,7 +37,7 @@ const FeaturedProfessions = () => {
                             Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
                         </div>
                     </div>
-                    <div className="col-1-4@xl padd-lg">
+                    <div className="col-1-4@xl col-1-4@lg col-1-4@md padd-lg">
                         <div className="ratio-3-2 bg-color-light"></div>
                         <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
                             <div className="position-middle-parent">
@@ -35,16 +46,7 @@ const FeaturedProfessions = () => {
                             Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
                         </div>
                     </div>
-                    <div className="col-1-4@xl padd-lg">
-                        <div className="ratio-3-2 bg-color-light"></div>
-                        <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
-                            <div className="position-middle-parent">
-                                <div className="icon-xl bg-color-main flex-center"><span>Icon</span></div>
-                            </div>                            
-                            Vivamus facilisis est iaculis neque ornare faucibus. Nulla sagittis turpis massa, eleifend sollicitudin nisl gravida id. Curabitur tortor ipsum, condimentum a sapien at, laoreet dignissim nisi.
-                        </div>
-                    </div>
-                    <div className="col-1-4@xl padd-lg">
+                    <div className="col-1-4@xl col-1-4@lg col-1-4@md padd-lg">
                         <div className="ratio-3-2 bg-color-light"></div>
                         <div className="padd-xl-t padd-lg-lr padd-lg-b border-top-color-main border-4x position-relative bg-color-light">
                             <div className="position-middle-parent">
