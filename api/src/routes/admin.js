ce76147af53fdee6f55c6125a48623ac9f1ddda8
svettorelo <sabrinavettorelo@gmail.com>
2021-10-28 23:09:56 -0300

    admin route and controller functions for tips and getAllUsers

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -2,24 +2,34 @@
 const {Router} = require("express");
 const router = Router();
 const {
+  getAllUsersAdmin,
   userDelete,
   userUpdate,
   appointmentDelete,
   appointmentUpdate,
   categoryCreate,
   categoryUpdate,
-  categoryDelete
+  categoryDelete,
+  postTips,
+  tipUpdate,
+  tipDelete
 } = require('../controllers/index.js');
 
+//GET ALL USERS
+router.get('/users', (req,res,next)=>{
+  getAllUsersAdmin()
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
 //USER DELETE
-router.delete('/user/:username',(req,res,next)=>{
+router.delete('/users/:username',(req,res,next)=>{
   let {username} = req.params;
   userDelete(username)
     .then(result => res.json(result))
     .catch(err => next(err));
 });
 //USER UPDATE
-router.put ('/user/:username',(req,res,next)=>{
+router.put('/users/:username',(req,res,next)=>{
   let {username} = req.params;
   let updateInfo = req.body;  // {name,email,password,isAdmin,etc} = req.body
   userUpdate(username,updateInfo)
@@ -35,7 +45,7 @@ router.delete('/appointment/:id',(req,res,next)=>{
     .catch(err => next(err));
 });
 //APPOINTMENT UPDATE
-router.put ('/appointment/:id',(req,res,next)=>{
+router.put('/appointment/:id',(req,res,next)=>{
   let {id} = req.params;
   let updateInfo = req.body;   // {date,status,etc} = req.body
   appointmentUpdate(id,updateInfo)
@@ -62,6 +72,29 @@ router.put('/category/:id',(req,res,next)=>{
 router.delete('/category/:id', (req,res,next)=>{
   let {id} = req.params;
   categoryDelete(id)
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+
+//TIP CREATE
+router.post('/tips',(req,res,next)=>{
+  let {text} = req.body;
+  postTips({text,isApproved:true})
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+//TIP UPDATE
+router.put('/tips/:id',(req,res,next)=>{
+  let {id} = req.params;
+  let updateInfo = req.body;
+  tipUpdate(id,updateInfo)
+    .then(result => res.json(result))
+    .catch(err => next(err));
+});
+//TIP DELETE
+router.delete('/tips/:id', (req,res,next)=>{
+  let {id} = req.params;
+  tipDelete(id)
     .then(result => res.json(result))
     .catch(err => next(err));
 });
