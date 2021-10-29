4c9a0e27b1c3f869a37a721d8fcc5c16a7992b48
Mauricio Portillo <maujpok@gmail.com>
2021-10-28 23:14:17 -0300

    API/rutas varias

--- api/src/controllers/functions/getUsersToForm.js
+++ api/src/controllers/functions/getUsersToForm.js
@@ -1,15 +1,5 @@
 const User = require('../../models/User');
 
-module.exports = async (req, res, next) => {
-    try{
-        const allUsers = await User.find({}, {_id: false, username: true, email: true})
-        return res.send(allUsers)
-        
-    }catch(err){
-        let error = {
-            message: 'Server error',
-            status: 500
-        }
-        next(error)
-    }
+module.exports = () => {
+    return User.find({}, {_id: false, username: true, email: true})
 };
--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,7 +1,39 @@
 const router = require ('express').Router();
-const {userCreate, getUsersToForm } = require('../controllers/index')
+const { userCreate, getUsersToForm, userDelete, userUpdate, getUserFullInfo } = require('../controllers/index')
 
-router.post('/', userCreate)
-router.get('/', getUsersToForm)
+router.post('/', (req, res, next) => {
+    const body = req.body;
+    userCreate(body)
+        .then(result => res.json(result))
+        .catch(err => next(err))
+});
 
-module.exports = router;
+router.get('/', (req, res, next) => {
+    getUsersToForm()
+        .then(result => res.json(result))
+        .catch(err => next(err))
+});
+
+router.get('/:username', (req, res, next) => {
+    const {username} = req.params;
+    getUserFullInfo(username)
+        .then(result => res.json(result))
+        .catch(err => next(err))
+});
+
+router.delete('/:username',(req,res,next)=>{
+    let {username} = req.params;
+    userDelete(username)
+      .then(result => res.json(result))
+      .catch(err => next(err))
+});
+
+router.put ('/:username',(req,res,next)=>{
+    let {username} = req.params
+    let updateInfo = req.body
+    userUpdate(username,updateInfo)
+      .then(result => res.json(result))
+      .catch(err => next(err))
+});
+
+module.exports = router; 
