c8891b284abe66e3b4dd8ab8f1b05c44e0f4be74
Mauricio Portillo <maujpok@gmail.com>
2021-10-29 16:28:53 -0300

    helpers folder

--- api/src/controllers/functions/userCreate.js
+++ api/src/controllers/functions/userCreate.js
@@ -1,9 +1,10 @@
 const User = require('../../models/User');
 
 module.exports = (body) => {
-    const {name, lastname, username, email, password, country, isProfessional, category, professionalRegistration} = body;
+    const {isAdmin, name, lastname, username, email, password, country, isProfessional, category, professionalRegistration} = body;
 
     let newUser = new User({
+        isAdmin,
         name,
         lastname,
         username,
