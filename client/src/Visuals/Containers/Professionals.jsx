b00f03989e52560f052ecd42fd27b91e0a6e0f2c
Agustin Ecker <amecker@hotmail.com>
2021-10-30 13:30:06 -0300

    falto este archivo

--- client/src/Visuals/Containers/Professionals.jsx
+++ client/src/Visuals/Containers/Professionals.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import SearchBarPro from '../Components/SearchBarPro/SearchBarPro'
 import ProfesionalsCatalog from '../Components/ProfessionalCard/ProfessionalsCatalog'
+import {useDispatch} from "react-redux"
+import { getAllProfs } from '../../Controllers/actions/professionalsActions'
 
 function Profesionals() {
+    const dispatch= useDispatch()
+    dispatch(getAllProfs())
     return (
         <div>
             Professionals Components Here
