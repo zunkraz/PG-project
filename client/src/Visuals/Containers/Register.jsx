26999bc8df652c7a3fa94bf3eca53f70eaf31992
Agustin Ecker <amecker@hotmail.com>
2021-10-30 16:47:04 -0300

    agregados labels al registro y acomodado mejor

--- client/src/Visuals/Containers/Register.jsx
+++ client/src/Visuals/Containers/Register.jsx
@@ -19,7 +19,6 @@ export default function Register(){
     }
     return(
         <div class="uk-background-muted">
-            <h2 class="uk-margin-left uk-padding">Registro</h2>
             <div class="uk-grid">
                 <button class="uk-button uk-button-danger uk-width-1-2" name="cliente" onClick={handleClick}>Nuevo Cliente</button>
                 <button class="uk-button uk-button-danger uk-width-1-2" name="profesional" onClick={handleClick}>Nuevo Profesional</button>
