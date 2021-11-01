fd7d59fcb51d1cc9e2b0ffb934f10d5f83966389
Gaston Scocco <gaston_scocco@hotmail.com>
2021-11-01 16:24:45 -0300

    /miperfil/:username Ready

--- client/src/Visuals/Components/PersonalDashboards/PersonalDashboardContainer.jsx
+++ client/src/Visuals/Components/PersonalDashboards/PersonalDashboardContainer.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import PersonalCardUser from './PersonalCardUser'
 
 function PersonalDashboardContainer({user}) {
+    console.log(user)
     return (
         <div className='bg-gray-100 rounded-3xl '>
                 <PersonalCardUser   img={user.img} 
                                     likes={user.likes} 
                                     dislikes={user.dislikes} 
-                                    sessions={user.category==='Normal'?user.timeOfSessions:user.visibility}
-                                    isVerified={user.isVerified}
-                                    category={user.category}
+                                    //sessions={user.category==='Normal'?user.timeOfSessions:user.visibility}
+                                    //isVerified={user.isVerified}
+                                    professional={user.isProfessional}
                     />
         </div>
     )
