66b25f8a46247d064b93350bceabda85b0409ee2
Nohelia <82003379+Noheliajoeliana@users.noreply.github.com>
2021-11-07 12:49:20 -0400

    deleted consolelog

--- api/src/controllers/functions/getReviews.js
+++ api/src/controllers/functions/getReviews.js
@@ -3,7 +3,6 @@ const User = require('./../../models/User')
 
 module.exports = ({userId}) => {
   if(userId){
-    console.log(userId)
     return Review.findOne({userId})
   }
   return Review.aggregate([
