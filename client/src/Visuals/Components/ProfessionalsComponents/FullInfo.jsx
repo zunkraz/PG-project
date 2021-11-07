199b24f954aefb953d3b011d40800cdf8763f847
Mauricio Portillo <maujpok@gmail.com>
2021-11-07 20:32:30 -0300

    fixed little bugs

--- client/src/Visuals/Components/ProfessionalsComponents/FullInfo.jsx
+++ client/src/Visuals/Components/ProfessionalsComponents/FullInfo.jsx
@@ -1,30 +1,26 @@
 import React from 'react'
-// import { FaMarker } from "react-icons/fa";
-// import BigButton from '../BigButton';
-// import PopContainer from '../PopContainer';
-// import EditDataComponent from './EditDataComponent';
-// import ShowData from './ShowData';
-
 
 export default function FullInfo({profData}) {
+
+    console.log(profData)
 
     return (
         <div className=''>
             <div className='padd-md-b font-main text-bold text-center- font-xl border-bottom-color-main'>
                 Información detallada
             </div>
-                <div className="padd-md-b font-main text-center- font-xl border-bottom-color-main">
+                <div className="padd-md-b font-main text-center- font-xl">
                     Sobre mí
                 </div>            
                 <div className='user-dashboard-info-tabs-content border-color-dark-a20 padd-lg'>
                     {profData.biography}
                 </div>
-                <div className="padd-md-b font-main text-center- font-xl border-bottom-color-main">
+                <div className="padd-md-b font-main text-center- font-xl">
                     Datos profesionales
                 </div>            
                 <div className='user-dashboard-info-tabs-content border-color-dark-a20 padd-lg'>
                     <div>
-                        Consultas atendidas: {profData.appointments.length}
+                        Consultas atendidas: {profData.appointments ? profData.appointments.length : 0}
                     </div>
                     <div>
                         Categoría: {profData.category.name}
