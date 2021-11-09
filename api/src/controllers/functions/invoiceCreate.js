62c2537857c682faad04d12fb78696498850c9a4
svettorelo <sabrinavettorelo@gmail.com>
2021-11-09 14:57:13 -0300

    fixing controller

--- api/src/controllers/functions/invoiceCreate.js
+++ api/src/controllers/functions/invoiceCreate.js
@@ -1,4 +1,4 @@
-const Invoice = require("../models/Invoice");
+const Invoice = require("../../models/Invoice");
 
 module.exports = (data) =>{
   const newInvoice = {
@@ -8,8 +8,9 @@ module.exports = (data) =>{
     payerID: data.payerID,
     totalCost: data.cart.reduce((total,session)=>{return total+session.price},0),
     schedules: data.cart.map(session => {return {_id:session.id}}),
-    description: data.cart.reduce((text,session)=>{return text + session.name + ', ' },
-      'Sesión/es con: ').slice(0,-2)+'.'
+    description: data.cart.reduce((text,session)=>{
+      if (text.includes(session.name)) return text
+      else return text + session.name + ', ' },'').slice(0,-2)
   }
   const newInv = new Invoice(newInvoice);
   return newInv.save();
