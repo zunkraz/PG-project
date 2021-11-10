9acaec71367e8d34e7ad61cf7c4b93c07a961874
svettorelo <sabrinavettorelo@gmail.com>
2021-11-10 14:53:41 -0300

    } error

--- api/src/controllers/functions/getAllUsersAdmin.js
+++ api/src/controllers/functions/getAllUsersAdmin.js
@@ -4,5 +4,5 @@ const User = require('../../models/User');
 module.exports = () => {
   return User.find()
     .sort({username:1})
-    .select('name username lastname email isProfessional isAdmin img _id isVerified}');
+    .select('name username lastname email isProfessional isAdmin img _id isVerified isActive');
 }
