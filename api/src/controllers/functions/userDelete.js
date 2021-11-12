b3146d42a72b42a0bf4d9d7ddcbc426778df8843
Mauricio <maujpok@gmail.com>
2021-11-12 18:11:06 -0300

    userdelete update

--- api/src/controllers/functions/userDelete.js
+++ api/src/controllers/functions/userDelete.js
@@ -1,6 +1,13 @@
 //admin function
 const User = require('../../models/User');
+const Schedule = require('../../models/Schedule');
+const Review = require('../../models/Review');
 
-module.exports = (username) => {
+module.exports = async (username) => {
+  const getId = await User.findOne({username}).then(data=>data._id)
+  const id = getId.toString()
+  
+  await Schedule.deleteMany({userId:id}).then(()=> console.log("schedules deleted"))
+  await Review.deleteOne({userId:id}).then(()=> console.log("review deleted"))
   return User.deleteOne({username},{new: true});
 }
--- client/src/Visuals/Components/ProfessionalsComponents/QuickInfo.jsx
+++ client/src/Visuals/Components/ProfessionalsComponents/QuickInfo.jsx
@@ -6,12 +6,12 @@ import ImageComponent from "../ImageComponent";
 
 export default function QuickInfo({data}) {
 
-    const {name, lastname, img, likes, dislikes, country, isProfessional, appointments, isVerified} = data;
+    const {name, lastname, username, img, likes, dislikes, country, isProfessional, appointments, isVerified} = data;
     
     return (
         <div>
             <div className='padd-md-b font-main text-bold text-center- font-xl border-bottom-color-main'>
-                {name} {lastname}
+                {username}
             </div>
             <div className="mrg-lg-t border-radius-sm overflow-hidden">
                 <ImageComponent img={img} ratio={"ratio-1-1"}/>
