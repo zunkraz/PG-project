d95bb238096612ef89241afcefaa0d540b96c326
Nohelia <nohelia.j.r.a@gmail.com>
2021-11-14 20:46:04 -0400

    typo

--- client/src/ApiReq/mails.js
+++ client/src/ApiReq/mails.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import {BASIC_URL} from '../constants'
 
-export function sendMail(type,headers) {
-  return axios.post(`${BASIC_URL}/emails?type=${type}`,headers).data;
+export function sendMail(type,body) {
+  return axios.post(`${BASIC_URL}/emails?type=${type}`,body).data;
 }
