d3826ebe12c07a5d6bcbedaa22e0e9df9050bd6b
Agustin Ecker <amecker@hotmail.com>
2021-11-15 22:55:23 -0300

    accent

--- client/src/Visuals/Components/FooterComponents/FooterContact.jsx
+++ client/src/Visuals/Components/FooterComponents/FooterContact.jsx
@@ -15,7 +15,7 @@ function FooterContact() {
     return (
         <React.Fragment>
             <div className="font-color-light font-main font-xl">
-                <span className="padd-md-b padd-lg-r font-bold border-bottom-color-main">Seguinos en redes!</span>
+                <span className="padd-md-b padd-lg-r font-bold border-bottom-color-main">Seguínos en redes!</span>
             </div>
             <ul className="vertical-list mrg-lg-t">
                 {
