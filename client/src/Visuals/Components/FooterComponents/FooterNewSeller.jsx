4429896f06899f3c7fe1184b3ac992b3eea4c4a7
nohel <nohelia.j.r.a@gmail.com>
2021-11-18 14:35:38 -0400

    mini changes

--- client/src/Visuals/Components/FooterComponents/FooterNewSeller.jsx
+++ client/src/Visuals/Components/FooterComponents/FooterNewSeller.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import logoFooter from "../../Assets/media/logo-main-footer.svg";
-import * as FaIcons from 'react-icons/fa';
+import { Link } from 'react-router-dom'
 
 function FooterNewSeller() {
     return (
@@ -11,12 +11,11 @@ function FooterNewSeller() {
             <div className="padd-lg-tb font-color-light">
                 <p>Resolvemos tus problemas de una manera eficiente y rapida, no dudes en realizar consultas en nuestra plataforma!</p>
             </div>
-            <div className='SellerSubmit* flex-center' data-uk-height-match>
-                <input name="mail" className="padd-md border-color-light border-4x" placeholder='Introduce tu mail'/>
-                <button className="bg-color-main icon-xl flex-center">
-                    <FaIcons.FaChevronRight className="font-xl font-color-light"/>
+            <Link to='/opinion' style={{ "textDecoration": "none" }}>
+                <button className="mrg-lg-t padd-md border-radius-sm action action-professional">
+                    Dejanos tu opinión
                 </button>
-            </div>
+            </Link>
         </div>
     )
 }
