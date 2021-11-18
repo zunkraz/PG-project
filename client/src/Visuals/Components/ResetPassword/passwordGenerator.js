f7228f58a667f76fd81bb8430d56a261ea8d049d
nohel <nohelia.j.r.a@gmail.com>
2021-11-17 22:24:56 -0400

    resetear password desde admin panel con correo

--- client/src/Visuals/Components/ResetPassword/passwordGenerator.js
+++ client/src/Visuals/Components/ResetPassword/passwordGenerator.js
@@ -1,5 +1,5 @@
 export default function generator(){
-    const characters = '!@#$%^&*()0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     let pass = ''
     for(let i = 0; i<=20;i++){
         pass+=characters[Math.floor(Math.random()*characters.length)]
