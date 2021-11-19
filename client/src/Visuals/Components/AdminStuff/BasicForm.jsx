b7e61c069a40f56316c5f8cdaf6bc23a125a414e
Mauricio <maujpok@gmail.com>
2021-11-18 23:55:41 -0300

    fixed warning

--- client/src/Visuals/Components/AdminStuff/BasicForm.jsx
+++ client/src/Visuals/Components/AdminStuff/BasicForm.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {adminSetCategCount, postAdminCategory,postAdminCountry,postAdminTip,putAdminCategory} from "../../../Controllers/actions/adminActions";
+import {postAdminCategory,postAdminCountry,postAdminTip,putAdminCategory} from "../../../Controllers/actions/adminActions";
 import Swal from 'sweetalert2';
 
 function BasicForm({component}){
